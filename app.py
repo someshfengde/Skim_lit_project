554875df913445c7e2a5008e14118329a1aa9d29
someshfengde <someshfengde@gmail.com>
2021-07-02 22:27:52 +0530

    asdfasd

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if text != None:
 
     for i in range(len(arr)):
         x,y = arr[i]
-        if x in final.keys() : 
+        if str(x) in final.keys() : 
             final[x] = final[x].append(y)
             st.write(f'appended this {y} to **{x}**')
         final[x] = [y]
